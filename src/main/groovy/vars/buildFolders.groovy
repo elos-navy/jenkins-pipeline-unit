acfa67d59a637be769908fdca0efea3b73cceb0d
Lukas Smiga <lukas.smiga@elostech.cz>
2021-01-22 09:58:19 +0100

    jenkins harness initial setup

---
+++ src/main/groovy/vars/buildFolders.groovy
@@ -0,0 +1,6 @@
+// Sample SharedLib containing job folder building logic
+
+folder("builder/testFolder") { // TODO change folder name to injected parameter
+    displayName("Project Test Folder")
+}
+
